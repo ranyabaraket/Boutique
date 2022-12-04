b224f5b1a99f5ae6851cd7c50b04171da680c342
Rania Baraket <raniabaraket2017@gmail.com>
2022-12-04 21:36:12 +0100

    add docker files

--- backend/config/key.js
+++ backend/config/key.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/myboutique",
+  mongoURI: "mongodb://localhost:27017/boutique",
   SECRET: process.env.APP_SECRET,
 };
